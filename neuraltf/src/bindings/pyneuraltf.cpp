8d20e251b9a3687137492dbd2b9e6da67d72b86d
Dominik Engel <dominik.engel@mail.de>
2023-07-06 14:10:23 +0200

    adapt NTFProperty bindings, i.e. remove simTf, add color_, isovalue, ...

--- neuraltf/src/bindings/pyneuraltf.cpp
+++ neuraltf/src/bindings/pyneuraltf.cpp
@@ -12,7 +12,7 @@ namespace inviwo {
 
 void exposeNTFBindings(py::module& m) {
         py::class_<NTFProperty, CompositeProperty>(m, "NTFProperty")
-        .def(py::init([](std::string_view identifier, std::string_view displayName, VolumeInport* inport, 
+        .def(py::init([](std::string_view identifier, std::string_view displayName, VolumeInport* inport,
                 InvalidationLevel invalidationLevel, PropertySemantics semantics) {
                  return new NTFProperty(identifier, displayName, inport, invalidationLevel, semantics);
              }),
@@ -25,12 +25,10 @@ void exposeNTFBindings(py::module& m) {
         .def("getAnnotatedVoxels", &NTFProperty::getAnnotatedVoxels)
         .def_property_readonly("tf", py::cpp_function([](NTFProperty& p) -> TransferFunctionProperty& { return p.tf_; },
                              py::return_value_policy::reference_internal))
-        .def_property_readonly("simtf", py::cpp_function([](NTFProperty& p) -> TransferFunctionProperty& { return p.simTf_; },
-                             py::return_value_policy::reference_internal))
         .def_property("modality", &NTFProperty::getModality, &NTFProperty::setModality)
         .def_property("modalityWeight", &NTFProperty::getModalityWeight, &NTFProperty::setModalityWeight)
-        .def_property("similarityExponent", &NTFProperty::getSimilarityExponent, &NTFProperty::setSimilarityExponent)
-        .def_property("similarityThreshold", &NTFProperty::getSimilarityThreshold, &NTFProperty::setSimilarityThreshold)
+        .def_property("isoValue", &NTFProperty::getIsoValue, &NTFProperty::setIsoValue)
+        .def_property("color", &NTFProperty::getColor, &NTFProperty::setColor)
         .def_property("similarityReduction", &NTFProperty::getSimilarityReduction, &NTFProperty::setSimilarityReduction)
         .def_property("requiresUpdate", &NTFProperty::getRequiresUpdate, &NTFProperty::setRequiresUpdate);
 }
